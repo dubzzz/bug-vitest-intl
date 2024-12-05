56984e4f8963690adffc2c681b2912ba51f7eb25
Nicolas DUBIEN <github@dubien.org>
2024-12-05 08:44:47 +0000

    add several examples

--- main.spec.js
+++ main.spec.js
@@ -2,7 +2,32 @@ import { test, vi } from "vitest";
 
 const UntouchedIntl = Intl;
 
-test("My test (not working)", () => {
+test("My test (not working #1)", () => {
+  const defaultLocale = "en-US";
+  vi.stubGlobal("Intl", {
+    ...UntouchedIntl,
+    NumberFormat: class extends UntouchedIntl.NumberFormat {
+      constructor(...args) {
+        if (args.length < 1) {
+          super(defaultLocale);
+        } else {
+          const [locales, ...otherParams] = args;
+          if (
+            typeof locales === "string" ||
+            (Array.isArray(locales) && locales.length !== 0)
+          ) {
+            super(...args);
+          } else {
+            super(defaultLocale, ...otherParams);
+          }
+        }
+      }
+    },
+  });
+  vi.useFakeTimers();
+});
+
+test("My test (not working #2)", () => {
   vi.stubGlobal("Intl", { ...UntouchedIntl });
   vi.useFakeTimers();
 });
