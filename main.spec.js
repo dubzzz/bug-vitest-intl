6ab943e695cbfa4676027726b8a518b6437e5ed2
Nicolas DUBIEN <github@dubien.org>
2024-12-05 08:42:05 +0000

    Basic repro

---
+++ main.spec.js
@@ -0,0 +1,28 @@
+import { test, vi } from "vitest";
+
+const UntouchedIntl = Intl;
+
+test("My test", () => {
+  const defaultLocale = "en-US";
+  vi.stubGlobal("Intl", {
+    ...UntouchedIntl,
+    NumberFormat: class extends UntouchedIntl.NumberFormat {
+      constructor(...args) {
+        if (args.length < 1) {
+          super(defaultLocale);
+        } else {
+          const [locales, ...otherParams] = args;
+          if (
+            typeof locales === "string" ||
+            (Array.isArray(locales) && locales.length !== 0)
+          ) {
+            super(...args);
+          } else {
+            super(defaultLocale, ...otherParams);
+          }
+        }
+      }
+    },
+  });
+  vi.useFakeTimers();
+});
